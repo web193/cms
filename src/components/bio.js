4a357a57d4b663b972f3e9fdf5a415e7825454b3
Yang <huiyang@m-c02l52z0f1g3.local>
2020-06-01 15:37:59 -0400

    inintal commit

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,8 +34,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in MI and is making stuff.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
